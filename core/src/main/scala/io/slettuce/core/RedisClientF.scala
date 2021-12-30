a06c108314e24d8a4633f2b2f94c75ee089d690d
naoh <naoh87@gmail.com>
2021-12-30 11:56:23 +0900

    [refine] RedisClientF

--- core/src/main/scala/io/slettuce/core/RedisClientF.scala
+++ core/src/main/scala/io/slettuce/core/RedisClientF.scala
@@ -9,6 +9,9 @@ import io.lettuce.core.cluster.api.StatefulRedisClusterConnection
 import io.lettuce.core.codec.RedisCodec
 import io.slettuce.core.util.JavaFutureUtil
 import cats.syntax.functor._
+import cats.syntax.flatMap._
+import io.lettuce.core.cluster.models.partitions.Partitions
+import io.slettuce.core.RedisClientF.ShutdownConfig
 import scala.reflect.ClassTag
 
 object RedisClientF {
@@ -16,29 +19,39 @@ object RedisClientF {
 
   def resource[F[_] : Async](
     make: => RedisClusterClient,
-    shutdown: ShutdownConfig = ShutdownConfig(0, 2, TimeUnit.SECONDS)
+    shutdownConfig: ShutdownConfig = ShutdownConfig(0, 2, TimeUnit.SECONDS)
   ): Resource[F, RedisClientF[F]] =
     Resource
-      .make(Sync[F].delay(make))(c =>
-        JavaFutureUtil.toAsync(
-          c.shutdownAsync(shutdown.quietPeriod, shutdown.timeout, shutdown.timeUnit)).void)
-      .map(c => new RedisClientF[F](c))
+      .make(
+        Sync[F]
+          .delay(new RedisClientF[F](make))
+          .flatTap(_.getPartition)
+      )(_.shutdownAsync(shutdownConfig))
 }
 
 class RedisClientF[F[_]](underlying: RedisClusterClient)(implicit F: Async[F]) {
   def connect[K, V: ClassTag](codec: RedisCodec[K, V]): Resource[F, RedisClusterConnectionF[F, K, V]] =
-    Resource.suspend(
-      F.blocking(underlying.getPartitions)
-        .map(_ =>
-          Resource
-            .make(
-              JavaFutureUtil.toAsync(underlying.connectAsync(codec)))(c =>
-              JavaFutureUtil.toAsync(c.closeAsync()).void)
-            .map(c => new RedisClusterConnectionF[F, K, V](c))))
+    Resource.make(connectRaw(codec))(_.closeAsync())
+
+  def connectRaw[K, V: ClassTag](codec: RedisCodec[K, V]): F[RedisClusterConnectionF[F, K, V]] =
+    JavaFutureUtil.toAsync(underlying.connectAsync(codec))
+      .map(c => new RedisClusterConnectionF(c))
+
+  def getPartition: F[Partitions] =
+    F.blocking(underlying.getPartitions)
+
+  def shutdownAsync(config: ShutdownConfig): F[Unit] =
+    shutdownAsync(config.quietPeriod, config.timeout, config.timeUnit)
+
+  def shutdownAsync(quietPeriod: Long, timeout: Long, timeUnit: TimeUnit): F[Unit] =
+    JavaFutureUtil.toAsync(underlying.shutdownAsync(quietPeriod, timeout, timeUnit)).void
 }
 
 class RedisClusterConnectionF[F[_] : Async, K, V: ClassTag](underlying: StatefulRedisClusterConnection[K, V]) {
   def async(): RedisClusterCommandsF[F, K, V] =
     new RedisClusterCommandsF[F, K, V](underlying.async())
+
+  def closeAsync(): F[Unit] =
+    JavaFutureUtil.toAsync(underlying.closeAsync()).void
 }
 
