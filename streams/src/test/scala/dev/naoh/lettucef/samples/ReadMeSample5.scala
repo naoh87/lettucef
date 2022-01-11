edb6bfc9f3ea2cecd564dabbfc844f30077cadc1
naoh <naoh87@gmail.com>
2022-01-11 11:51:22 +0900

    [update] async api usage sample

--- streams/src/test/scala/dev/naoh/lettucef/samples/ReadMeSample5.scala
+++ streams/src/test/scala/dev/naoh/lettucef/samples/ReadMeSample5.scala
@@ -12,17 +12,19 @@ object ReadMeSample5 extends IOApp.Simple {
     for {
       client <- LettuceF.cluster[IO](RedisClusterClient.create("redis://127.0.0.1:7000"))
       conn <- client.connect(StringCodec.UTF8)
+      conn2 <- client.connect(StringCodec.UTF8)
       sync = conn.sync()
       async = conn.async()
     } yield for {
       start <- IO(System.nanoTime())
-      _ <- sync.set("Ix", "0")
+      _ <- async.set("Ix", "0")
       _ <- async.incr("Ix").replicateA_(100000)
+      _ <- conn2.sync().get("Ix").flatTap(IO.println) // Some(6453)
       aget <- async.get("Ix")
-      sget <- sync.get("Ix")
-      aget <- aget
+      _ <- sync.get("Ix").flatTap(IO.println) // Some(1000000)
+      _ <- aget.flatTap(IO.println) // Some(1000000)
       end <- IO(System.nanoTime())
     } yield
-      println((sget, aget, (end - start).nanos.toMillis)) //(Some(100000),Some(100000),3338)
+      println((end - start).nanos.toMillis) // 3320
   }.use(identity)
 }
