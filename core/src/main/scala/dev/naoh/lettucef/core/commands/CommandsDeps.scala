f4f405014ec388581a0393cc342a64eea2c139e8
naoh <naoh87@gmail.com>
2022-01-19 09:58:54 +0900

    [fix] CommandsDeps

--- core/src/main/scala/dev/naoh/lettucef/core/commands/CommandsDeps.scala
+++ core/src/main/scala/dev/naoh/lettucef/core/commands/CommandsDeps.scala
@@ -6,8 +6,5 @@ import dev.naoh.lettucef.core.util.ManualDispatchHelper
 trait CommandsDeps[F[_], K, V] {
   implicit protected val _async: Async[F]
 
-  protected val underlying: Any
-
   protected val dispatchHelper: ManualDispatchHelper[K, V]
 }
-
