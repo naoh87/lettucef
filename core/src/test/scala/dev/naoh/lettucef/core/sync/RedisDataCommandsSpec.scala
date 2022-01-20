056ff8b938217d1c9f7b5afc25676307dc351be8
naoh <naoh87@gmail.com>
2022-01-20 09:38:23 +0900

    [fix] remove unused import

--- core/src/test/scala/dev/naoh/lettucef/core/sync/RedisDataCommandsSpec.scala
+++ core/src/test/scala/dev/naoh/lettucef/core/sync/RedisDataCommandsSpec.scala
@@ -1,25 +1,13 @@
 package dev.naoh.lettucef.core.sync
 
 import cats.effect.IO
-import cats.effect.Resource
-import cats.effect.std.Dispatcher
-import cats.effect.std.Queue
-import cats.syntax.traverse._
 import dev.naoh.lettucef.api.models.RedisData
-import dev.naoh.lettucef.api.models.RedisData.RedisArray
-import dev.naoh.lettucef.api.models.pubsub.RedisPushed
-import dev.naoh.lettucef.api.models.pubsub.RedisPushed.Unsubscribed
-import dev.naoh.lettucef.api.models.pubsub.RedisPushed._
-import dev.naoh.lettucef.core.RedisPubSubF
-import dev.naoh.lettucef.core.sync.RedisTest.RedisKey
-import dev.naoh.lettucef.core.sync.RedisTest.RedisValue
 import io.lettuce.core.AclSetuserArgs
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
 class RedisDataCommandsSpec extends AnyFreeSpec with Matchers {
 
-  import RedisTest.syntax._
 
   "check" in RedisTest.commands { sync =>
     def notEmpty[R](data: List[RedisData[R]]): IO[Unit] =
