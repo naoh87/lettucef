9b5d075ad9243999dd2b81be1a0b83b3155b88d8
naoh <naoh87@gmail.com>
2022-01-21 12:01:30 +0900

    [update] ConnectionPool Sample

--- README.md
+++ README.md
@@ -211,30 +211,32 @@ val printResource: Resource[IO, Int] =
 def run: IO[Unit] = {
   for {
     client <- LettuceF.cluster[IO](RedisClusterClient.create("redis://127.0.0.1:7000"))
-    pub <- client.connect(StringCodec.UTF8).flatMap(printResource.as).map(_.sync())
+    pub <- client.connect(StringCodec.UTF8).map(_.sync())
     pool <- client.connect(StringCodec.UTF8).map(_.sync())
             .flatMap(printResource.as)
             .pipe(ResourcePool(maxIdle = 2, minIdle = 1).make(_))
   } yield for {
     _ <- pub.unlink("hoge")
     _ <- List.range(0, 3).map(i => pub.rpush("hoge", i.toString)).sequence
-    _ <- IO.println("rpushed")
-    a <- List.fill(4)(pool.use(_.blpop(1, "hoge")).start).sequence
-    _ <- a.map(_.joinWithNever).sequence.flatTap(IO.println)
+    _ <- IO.println("start")
+    _ <- pool.use(_.blpop(1, "hoge").flatTap(IO.println)).parReplicateA(4)
+    _ <- IO.println("end")
   } yield ()
 }.use(identity)
 // 0 >
+// start          # active=0 idle=1
 // 1 >
-// rpushed
 // 2 >
 // 3 >
-// 4 >
-// 3 <
-// 2 <
-// List(Some((hoge,1)), None, Some((hoge,0)), Some((hoge,2)))
+// Some((hoge,0)) # active=4 idle=0
+// Some((hoge,2)) # active=3 idle=1
+// Some((hoge,1)) # active=2 idle=2
 // 1 <
-// 4 <
+// None           # active=1 idle=2
+// 3 <            
+// end            # active=0 idle=2
 // 0 <
+// 2 <
 ```
 
 # Features
--- examples/src/main/scala/com/example/ConnectionPoolExample.scala
+++ examples/src/main/scala/com/example/ConnectionPoolExample.scala
@@ -5,6 +5,7 @@ import cats.effect.IO
 import cats.effect.IOApp
 import cats.effect.Resource
 import cats.syntax.functor._
+import cats.syntax.parallel._
 import cats.syntax.traverse._
 import dev.naoh.lettucef.api.LettuceF
 import dev.naoh.lettucef.api.extras.ResourcePool
@@ -20,28 +21,30 @@ object ConnectionPoolExample extends IOApp.Simple {
   override def run: IO[Unit] = {
     for {
       client <- LettuceF.cluster[IO](RedisClusterClient.create("redis://127.0.0.1:7000"))
-      pub <- client.connect(StringCodec.UTF8).flatMap(printResource.as).map(_.sync())
+      pub <- client.connect(StringCodec.UTF8).map(_.sync())
       pool <- client.connect(StringCodec.UTF8).map(_.sync())
         .flatMap(printResource.as)
         .pipe(ResourcePool(maxIdle = 2, minIdle = 1).make(_))
     } yield for {
       _ <- pub.unlink("hoge")
       _ <- List.range(0, 3).map(i => pub.rpush("hoge", i.toString)).sequence
-      _ <- IO.println("rpushed")
-      a <- List.fill(4)(pool.use(_.blpop(1, "hoge")).start).sequence
-      _ <- a.map(_.joinWithNever).sequence.flatTap(IO.println)
+      _ <- IO.println("start")
+      _ <- pool.use(_.blpop(1, "hoge").flatTap(IO.println)).parReplicateA(4)
+      _ <- IO.println("end")
     } yield ()
   }.use(identity)
   // 0 >
+  // start
   // 1 >
-  // rpushed
   // 2 >
   // 3 >
-  // 4 >
-  // 3 <
-  // 2 <
-  // List(Some((hoge,1)), None, Some((hoge,0)), Some((hoge,2)))
+  // Some((hoge,0))
+  // Some((hoge,2))
+  // Some((hoge,1))
   // 1 <
-  // 4 <
+  // None
+  // 3 <
+  // end
   // 0 <
+  // 2 <
 }
